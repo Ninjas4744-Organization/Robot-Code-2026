72dd20ab597788074b7b3e831f6c7bdc648e39df
DirtyDan16 <138337745+DirtyDan16@users.noreply.github.com>
2026-01-13 23:21:45 +0200

    Added the different robot states names in States.java

--- src/main/java/frc/robot/States.java
+++ src/main/java/frc/robot/States.java
@@ -1,8 +1,30 @@
 package frc.robot;
 
 public enum States {
-    STARTING_POSE,
-    UNKNOWN,
-    RESET,
-    IDLE
+     STARTING_POSE,
+     IDLE,
+     RESET,
+
+     //Intake / Delivery
+     INTAKE,
+     INTAKE_WHILE_DELIVERY_HEATING,
+     INTAKE_WHILE_DELIVERY,
+     DELIVERY_HEATED,
+     DELIVERY,
+     DUMP,
+
+     //Shooting
+     SHOOT_HEATED,
+     SHOOT_READY,
+     SHOOT,
+     INTAKE_WHILE_SHOOT_HEATED,
+
+     //Climbing
+     CLIMB1_READY,
+     CLIMB1,
+     CLIMB_DOWN,
+     CLIMB2_READY,
+     CLIMB2,
+     CLIMB3_READY,
+     CLIMB3
 }
