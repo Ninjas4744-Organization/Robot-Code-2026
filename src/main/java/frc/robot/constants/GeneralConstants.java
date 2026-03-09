d2862ec837732b7a225c1fb784b5e7e821c7a9ba
Ninjas4744 <ninjas4744@gmail.com>
2026-03-09 03:49:09 +0200

    kDriverRotationPowFactor = 0.75

--- src/main/java/frc/robot/constants/GeneralConstants.java
+++ src/main/java/frc/robot/constants/GeneralConstants.java
@@ -54,7 +54,7 @@ public class GeneralConstants {
         public static double kDriverSpeedFactor = 1;
         public static double kDriverRotationSpeedFactor = 1;
         public static double kDriverPowFactor = 0.75;
-        public static double kDriverRotationPowFactor = 0.5;
+        public static double kDriverRotationPowFactor = 0.75;
         public static double kJoystickDeadband = 0.04;
         public static boolean kDriverFieldRelative = true;
     }
